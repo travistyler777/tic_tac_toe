915295b9776c45c6531b147a9962375f5c4fddff
Travis <travis@mooremediallc.com>
2023-02-02 19:04:59 -0700

    Add player controls

--- scripts.js
+++ scripts.js
@@ -2,19 +2,90 @@
 //It also needs to check if I made three squares in a row. 
 //If it did, then announce winner, if not switch players
 
-
-const gameBoard = (squares) => {
-    const boardContainer = document.querySelector("#gameboard-container");
-    
-    //Create squares
-    for (let i = 0; i < squares; i++){
-        const boardSquare = document.createElement('div');
-        boardSquare.classList.add('square');
-        boardContainer.appendChild(boardSquare);
-    }
+gameBoard = {
+    gameboard: [{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''}]
 }
 
-gameBoard(9)
+const init = () => {
+  
+}
+
+
+const renderBoard = () => {
+    
+    const boardContainer = document.querySelector('#gameboard-container');
+
+    //Render Squares
+    gameBoard.gameboard.forEach((value, index) => {
+        const boardSquare = document.createElement('div');
+        boardSquare.setAttribute('id', `square${index}`);
+        boardSquare.setAttribute('class', 'square');
+        boardContainer.appendChild(boardSquare);
+        boardSquare.textContent = value.square;
+    })
+}
+
+const boardController = () => {
+    
+    const boardSquare = document.querySelectorAll('.square')
+    let turnCount = 0
+
+    boardSquare.forEach((square, index) => {
+        square.addEventListener('click', () => {
+            console.log(index)
+            square.textContent = playerToggle(turnCount); 
+            turnCount++    
+        })
+    })
+}
+
+const playerToggle = (turnCount) => {
+    if (turnCount % 2 === 0)
+    {
+        return 'X'
+    }
+    else {
+        return 'O'
+    }
+
+}
+
+
+const checkWinner =(player, marker) => {
+
+    // if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[1].square === marker && gameBoard.gameboard[2].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[3].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[5].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[6].square === marker && gameBoard.gameboard[7].square === marker && gameBoard.gameboard[8].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[3].square === marker && gameBoard.gameboard[6].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[1].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[7].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[5].square === marker && gameBoard.gameboard[8].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[8].square === marker)
+    // {console.log(player + " is the winner!");}
+    // else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[6].square === marker)
+    // {console.log(player + " is the winner!");}
+}
+
+const createPlayer = (name, marker) => {
+    return{name, marker}
+}
+
+
+const player1 = createPlayer('Travis', 'X');
+const player2 = createPlayer('Bre', 'O');
+
+console.log(player1.name)
+
+renderBoard()  
+boardController()
+
+
+
 
 
 
