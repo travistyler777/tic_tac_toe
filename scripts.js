3912b65b24a6f8ec123cfc887f39abd48163aac2
Travis <travis@mooremediallc.com>
2023-02-04 09:13:08 -0700

    refactor

--- scripts.js
+++ scripts.js
@@ -6,10 +6,22 @@ gameBoard = {
     gameboard: [{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''},{square: ''}]
 }
 
-const init = () => {
-  
+const createPlayer = (name, marker) => {
+    return{name, marker}
 }
 
+const player1Input = document.querySelector('.player1-input');
+const player2Input = document.querySelector('.player2-input');
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const player1 = createPlayer(player1Input.value, 'X');
+    const player2 = createPlayer(player2Input.value, 'O');
+
+
+
+})
 
 const renderBoard = () => {
     
@@ -26,60 +38,51 @@ const renderBoard = () => {
 }
 
 const boardController = () => {
-    
     const boardSquare = document.querySelectorAll('.square')
     let turnCount = 0
 
     boardSquare.forEach((square, index) => {
         square.addEventListener('click', () => {
-            console.log(index)
-            square.textContent = playerToggle(turnCount); 
-            turnCount++    
+            console.log("Index: " + index)
+            square.textContent = playerToggle(turnCount)
+            gameBoard.gameboard[index].square = playerToggle(turnCount)
+            checkWinner('Travis', 'X')  
+            console.log(gameBoard.gameboard)
+            turnCount++  
         })
     })
 }
 
 const playerToggle = (turnCount) => {
-    if (turnCount % 2 === 0)
-    {
-        return 'X'
-    }
-    else {
-        return 'O'
-    }
+    return (turnCount % 2 === 0) ? 'X' : 'O';
 
 }
 
 
 const checkWinner =(player, marker) => {
 
-    // if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[1].square === marker && gameBoard.gameboard[2].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[3].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[5].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[6].square === marker && gameBoard.gameboard[7].square === marker && gameBoard.gameboard[8].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[3].square === marker && gameBoard.gameboard[6].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[1].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[7].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[5].square === marker && gameBoard.gameboard[8].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[8].square === marker)
-    // {console.log(player + " is the winner!");}
-    // else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[6].square === marker)
-    // {console.log(player + " is the winner!");}
-}
-
-const createPlayer = (name, marker) => {
-    return{name, marker}
+    if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[1].square === marker && gameBoard.gameboard[2].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[3].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[5].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[6].square === marker && gameBoard.gameboard[7].square === marker && gameBoard.gameboard[8].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[3].square === marker && gameBoard.gameboard[6].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[1].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[7].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[5].square === marker && gameBoard.gameboard[8].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[0].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[8].square === marker)
+    {console.log(player + " is the winner!");}
+    else if(gameBoard.gameboard[2].square === marker && gameBoard.gameboard[4].square === marker && gameBoard.gameboard[6].square === marker)
+    {console.log(player + " is the winner!");}
 }
 
 
-const player1 = createPlayer('Travis', 'X');
-const player2 = createPlayer('Bre', 'O');
 
-console.log(player1.name)
+
+
 
 renderBoard()  
 boardController()
