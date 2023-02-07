bb2c376e74531fc6a982105e1f5373b2e46cfff1
Travis <travis@mooremediallc.com>
2023-02-07 07:48:56 -0700

    code disableSquares funtion

--- scripts.js
+++ scripts.js
@@ -1,32 +1,42 @@
 const Gameboard = (() => {
     const gameboard = []
+    const winningMoves = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
     const setGameboard = (square, marker) => gameboard.push({square, marker}) 
     const getGameboard = () => gameboard;
 
-    const checkWinner = () => {
+    const checkWinner = (boardmoves, winningmoves) => {
 
-        const winningMoves = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,9],
-            [1,4,7],
-            [2,5,8],
-            [3,6,9],
-            [1,5,9],
-            [3,5,7]
-        ];
+        //Maps out move numbers in array
+        const xBoardMoves = boardmoves.map((index) => index).filter((items) => items.marker === 'X')
+        const xBoardMovesMap = xBoardMoves.map((index) => index.square);
 
-        
-        console.log(winningMoves[0]);
-        console.log(gameboard.map(index => index.square))
+        const oBoardMoves = boardmoves.map((index) => index).filter((items) => items.marker === 'O')
+        const oBoardMovesMap = xBoardMoves.map((index) => index.square);
 
-        console.log(gameboard.diff(winningMoves[0]));
+        //Checks both arrays for winning combos
+        const xHasWinningCombination = winningmoves.some(winningCombination =>
+            winningCombination.every(value => xBoardMovesMap.includes(value))
+        );
+        const oHasWinningCombination = winningmoves.some(winningCombination =>
+            winningCombination.every(value => oBoardMovesMap.includes(value))
+        );
 
+        //Returns results of matching items
+        if (xHasWinningCombination) {
+            return "XXXXXX winning combination!";
+        } 
+        else if(oHasWinningCombination) 
+        {
+            return "OOOOOO winning combination!";
+        }
+        else {
+            return "No winning combination yet.";
+        }   
     }
 
-
     return {
+        winningMoves,
         setGameboard,
         getGameboard,
         checkWinner
@@ -65,8 +75,8 @@ const boardController = (() => {
             square.textContent = activePlayer.marker;
             Gameboard.setGameboard(index, activePlayer.marker)
             toggleActivePlayer()
-            Gameboard.checkWinner()
-            
+            Gameboard.checkWinner(Gameboard.getGameboard(), Gameboard.winningMoves)
+            console.log(Gameboard.checkWinner(Gameboard.getGameboard(), Gameboard.winningMoves))
             
         })
     })
